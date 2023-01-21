b8a479cf81ab956803e5282634796331479f8bc7
Tazim381 <jucse28.381@gmail.com>
2023-01-21 15:46:01 +0600

    row-column-added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
 function addProduct() {
-    alert("added");
+  const id = document.getElementById("id").value;
+  const name = document.getElementById("name").value;
+  const price = document.getElementById("price").value;
+  var addrw = document.getElementById("table");
+  var newrw = addrw.insertRow(1);
+
+  var cel1 = newrw.insertCell(0);
+  var cel2 = newrw.insertCell(1);
+  var cel3 = newrw.insertCell(2);
+
+  cel1.innerHTML = id;
+  cel2.innerHTML = name;
+  cel3.innerHTML = price;
+  alert("added");
+}
+
+function resetAll() {
+    document.getElementById("inputProduct").reset();
 }
