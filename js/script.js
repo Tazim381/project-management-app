5c877761f05c8788f93ad541ceb71b1b2f1a55c0
Tazim381 <jucse28.381@gmail.com>
2023-01-22 01:10:24 +0600

    validation added

--- js/script.js
+++ js/script.js
@@ -1,17 +1,49 @@
-const itemText =[];
+const itemText = [];
+
+function checkValidity(id, name, price) {
+  if (id === "" || name === "" || price === "") {
+    alert("Emty string is not allowed");
+    return 0;
+  }
+  if (price < 0) {
+    alert("price should be a positive number");
+    return 0;
+  }
+  if (price > 100000) {
+    alert("price must be less than  100000 ");
+    return 0;
+  }
+  if (name.length > 60) {
+    alert("product name should be less than 60 character");
+    return 0;
+  }
+}
+
+function constractObjectArray(id,name,price) {
+  let item = {
+    p_id: id,
+    p_name: name,
+    p_price: price,
+  };
+  itemText.push(item);
+}
 
 function addProduct() {
   const id = document.getElementById("id").value;
   const name = document.getElementById("name").value;
   const price = document.getElementById("price").value;
- 
-  let item = {
-    p_id:id,
-    p_name:name,
-    p_price: price
-  };
 
-  itemText.push(item);
+  if ((checkValidity(id, name, price))==0) {
+    return;
+  }
+
+  constractObjectArray(id,name,price);
+
+  console.log(itemText);
+
+  
+
+
 
   var addrw = document.getElementById("table");
   var newrw = addrw.insertRow(1);
@@ -27,5 +59,5 @@ function addProduct() {
 }
 
 function resetAll() {
-    document.getElementById("inputProduct").reset();
+  document.getElementById("inputProduct").reset();
 }
