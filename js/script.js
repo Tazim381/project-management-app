6b44820d680be9089998ca56322616eefe5b3d93
Tazim381 <jucse28.381@gmail.com>
2023-01-22 02:41:38 +0600

    all validation part added

--- js/script.js
+++ js/script.js
@@ -1,31 +1,60 @@
 const itemText = [];
 
-function checkValidity(id, name, price) {
+function showError(value) {
+  const id = document.getElementById(value);
+  id.style.border = "2px solid";
+  id.style.borderColor = "red";
+  const valueError = value + "Error";
+  console.log(valueError);
+  const error = document.getElementById(valueError);
+  error.textContent = `Please insert correct ${value}`;
+  error.style.color = "red";
+}
+
+function checkInputValidity(id, name, price) {
   if (id === "" || name === "" || price === "") {
     alert("Emty string is not allowed");
     return 0;
   }
   if (price < 0) {
+    showError("price");
     alert("price should be a positive number");
     return 0;
   }
+
   if (price > 100000) {
+    showError("price");
     alert("price must be less than  100000 ");
     return 0;
   }
   if (name.length > 60) {
+    showError("name");
     alert("product name should be less than 60 character");
     return 0;
   }
 }
 
-function constractObjectArray(id,name,price) {
+function constractObjectArray(id, name, price) {
   let item = {
-    p_id: id,
-    p_name: name,
-    p_price: price,
+    productId: id,
+    productName: name,
+    productPrice: price,
   };
   itemText.push(item);
+}
+
+function checkUniqueId(id, itemText) {
+  const result = 1;
+  itemText.forEach(function (arrayItem) {
+    var x = arrayItem.productId;
+    if (x === id) {
+      showError("id");
+      alert("Id already exist");
+      result = 0;
+      return;
+    }
+  });
+  return result;
 }
 
 function addProduct() {
@@ -33,17 +62,15 @@ function addProduct() {
   const name = document.getElementById("name").value;
   const price = document.getElementById("price").value;
 
-  if ((checkValidity(id, name, price))==0) {
+  if (checkInputValidity(id, name, price) == 0) {
     return;
   }
 
-  constractObjectArray(id,name,price);
+  if (checkUniqueId(id, itemText) == 0) {
+    return;
+  }
 
-  console.log(itemText);
-
-  
-
-
+  constractObjectArray(id, name, price);
 
   var addrw = document.getElementById("table");
   var newrw = addrw.insertRow(1);
