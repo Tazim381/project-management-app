6a8f9dc2d1e9f6622cb6b9297c199a05741856f3
Tazim381 <jucse28.381@gmail.com>
2023-01-22 22:01:19 +0600

    delete button added

--- js/script.js
+++ js/script.js
@@ -5,10 +5,19 @@ function showError(value) {
   id.style.border = "2px solid";
   id.style.borderColor = "red";
   const valueError = value + "Error";
-  console.log(valueError);
   const error = document.getElementById(valueError);
   error.textContent = `Please insert correct ${value}`;
   error.style.color = "red";
+}
+
+function dontShowError(value) {
+  const id = document.getElementById(value);
+  id.style.border = "1px solid";
+  id.style.borderColor = "black";
+  const valueError = value + "Error";
+  const error = document.getElementById(valueError);
+  error.textContent = ``;
+  error.style.color = "";
 }
 
 function checkInputValidity(id, name, price) {
@@ -23,17 +32,24 @@ function checkInputValidity(id, name, price) {
     showError("price");
     alert("price should be a positive number");
     return 0;
+  } else {
+    dontShowError("price");
   }
 
   if (price > 100000) {
     showError("price");
     alert("price must be less than  100000 ");
     return 0;
+  } else {
+    dontShowError("price");
   }
+
   if (name.length > 60) {
     showError("name");
     alert("product name should be less than 60 character");
     return 0;
+  } else {
+    dontShowError("name");
   }
 }
 
@@ -55,11 +71,39 @@ function checkUniqueId(id, itemText) {
       alert("Id already exist");
       result = 0;
       return;
+    } else {
+      dontShowError("id");
     }
   });
   return result;
 }
 
+function insertItem() {
+  var addrw = document.getElementById("table");
+  var tableRows = addrw.getElementsByTagName('tr');
+  var rowCount = tableRows.length;
+  console.log(rowCount);
+  for (var x=rowCount-1; x>0; x--) {
+     document.getElementById("table").deleteRow(x);
+  }
+ 
+
+  itemText.forEach(function (arrayItem) {
+  var newrw = addrw.insertRow(1);
+
+  var cel1 = newrw.insertCell(0);
+  var cel2 = newrw.insertCell(1);
+  var cel3 = newrw.insertCell(2);
+  var cel4 = newrw.insertCell(3);
+  var cel5 = newrw.insertCell(4);
+  
+    cel1.innerHTML = arrayItem.productId;
+    cel2.innerHTML = arrayItem.productName;
+    cel3.innerHTML = arrayItem.productPrice;
+    cel4.innerHTML = "<button>Edit</button>";
+    cel5.innerHTML = `<button onclick='deleteItem(${arrayItem.productId})'>Delete</button>`;
+  });
+}
 function addProduct() {
   const id = document.getElementById("id").value;
   const name = document.getElementById("name").value;
@@ -75,19 +119,29 @@ function addProduct() {
 
   constractObjectArray(id, name, price);
 
-  var addrw = document.getElementById("table");
-  var newrw = addrw.insertRow(1);
+  
+  insertItem();
 
-  var cel1 = newrw.insertCell(0);
-  var cel2 = newrw.insertCell(1);
-  var cel3 = newrw.insertCell(2);
-
-  cel1.innerHTML = id;
-  cel2.innerHTML = name;
-  cel3.innerHTML = price;
   return false;
 }
 
 function resetAll() {
   document.getElementById("inputProduct").reset();
+}
+
+function deleteItem(id) {
+  alert(id);
+  let newId = id.toString();
+  let iterator = 0;
+  itemText.forEach(function (arrayItem) {
+    const x = arrayItem.productId;
+    if (x === newId) {
+      console.log("yes");
+      console.log(iterator);
+      itemText.splice(iterator, 1);
+      console.log(itemText);
+      insertItem();
+    }
+    iterator++;
+  });
 }
